c3fe46afa906feb038a925a93accc15cfd16de06
Lucas Chagas <lucaswillian2100@gmail.com>
2020-12-22 11:08:54 -0300

    exercicio de botoes como interruptor

--- Arduino Básico/BotaoComunicaSerial/BotaoComunicaSerial.ino
+++ Arduino Básico/BotaoComunicaSerial/BotaoComunicaSerial.ino
@@ -3,15 +3,38 @@ Se o botão estiver apertado, o monitor serial imprimirá 1.
 Senão, ele imprimirá 0.
 */
 
-const int botao = 53;         // pino no qual o botao está conectado
-int estadoBotao = 0;         // variável para guardar o valor do estado do botão (pressionado ou não)      
+const int botao1 = 43;         // pino no qual o botao está conectado
+const int botao2 = 51;
+const int led = 30;           // pino no qual o led está conectado
+int estadoBotao1 = 0;          // variável para guardar o valor do estado do botão1 (pressionado ou não)      
+int estadoBotao2 = 0;          // variável para guardar o valor do estado do botão2 (pressionado ou não) 
+int estadoLed = 0;
 
 void setup() {               // configuração inicial
   Serial.begin(9600);        // inicialização do monitor serial na baudrates de 9600 
-  pinMode(botao, INPUT);     // inicialização do pino do botão como entrada (INPUT)
+  pinMode(botao1, INPUT);     // inicialização do pino do botão como entrada (INPUT)
+  pinMode(botao2, INPUT);     // inicialização do pino do botão como entrada (INPUT)
+  pinMode(led, OUTPUT);     // inicialização do pino do led como saida (OUTPUT)
 }
 
 void loop() {                         //loop: tudo que estiver dentro se repetirá indefinidamente 
-   estadoBotao = digitalRead(botao);  // Lê o estado do botão (que pode ser 0 ou 1)
-   Serial.println(estadoBotao);       // imprime o estado do botão no monitor serial
+  delay(200);
+  //condicional se e senao
+  //Serial.println(digitalRead(botao1));
+  estadoBotao1 = digitalRead(botao1);
+  estadoBotao2 = digitalRead(botao2);
+  if (estadoBotao1 == 1){
+    estadoLed = 1;              
+    Serial.println("\nestadoLed On");      
+  }
+  if (estadoBotao2 == 1){
+    estadoLed = 0;              
+    Serial.println("\nestadoLed off");      
+  } 
+  if (estadoLed == 1){
+    digitalWrite(led,1);
+  }
+  else{
+    digitalWrite(led,0);
+  }
 }
